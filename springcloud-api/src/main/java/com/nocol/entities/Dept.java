f855059ba49f23f0b9ee1d3c7aaa29714d7275d2
nocol0609 <nkzx1996@163.com>
2019-08-25 23:45:33 +0800

    entity 添加注釋

--- springcloud-api/src/main/java/com/nocol/entities/Dept.java
+++ springcloud-api/src/main/java/com/nocol/entities/Dept.java
@@ -19,9 +19,20 @@ import java.io.Serializable;
 @Accessors(chain = true)  //链式风格访问，设置为true，则setter方法返回当前对象
 public class Dept implements Serializable {
 
-    private Long 	deptno; // 主键
-    private String 	dname; // 部门名称
-    private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+    /**
+     * 主键
+     */
+    private Long 	deptno;
+
+    /**
+     * 部门名称
+     */
+    private String 	dname;
+
+    /**
+     * 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+     */
+    private String 	db_source;
 
     public Dept(String dname) {
         super();
