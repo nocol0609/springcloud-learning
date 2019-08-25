54141640d45633dd01b2b553af6efabaca14d8f1
nocol0609 <nkzx1996@163.com>
2019-08-25 16:26:42 +0800

    创建api module工程、引入lombok

---
+++ springcloud-api/src/main/java/com/nocol/entity/Dept.java
@@ -0,0 +1,30 @@
+package com.nocol.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @author liuxp
+ * @version 1.0
+ * @date 2019/8/25 15:43
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)  //链式风格访问，设置为true，则setter方法返回当前对象
+public class Dept implements Serializable {
+
+    private Long 	deptno; // 主键
+    private String 	dname; // 部门名称
+    private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+    public Dept(String dname) {
+        super();
+        this.dname = dname;
+    }
+}
