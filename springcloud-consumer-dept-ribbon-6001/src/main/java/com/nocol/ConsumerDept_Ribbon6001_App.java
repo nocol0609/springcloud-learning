1891f06f653e0b83620ed8c1d123ac7ad4ebcfd8
liuxp <liuxp@nala.com.cn>
2019-08-26 18:23:16 +0800

    ribbon consumer

---
+++ springcloud-consumer-dept-ribbon-6001/src/main/java/com/nocol/ConsumerDept_Ribbon6001_App.java
@@ -0,0 +1,30 @@
+package com.nocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient  //向服务中心注册
+public class ConsumerDept_Ribbon6001_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerDept_Ribbon6001_App.class, args);
+    }
+
+    /**
+     * 向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
