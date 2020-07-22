8817ee561a01bfa42a0b48c1b620e4efd603b50b
nocol <nkzx1996@163.com>
2020-07-22 22:24:40 +0800

    eureka 集群配置

--- springcloud-provider-dept-8001/src/main/java/com/nocol/ProviderDept8001_App.java
+++ springcloud-provider-dept-8001/src/main/java/com/nocol/ProviderDept8001_App.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中，该注解表示自己是 eureka client
-//@EnableDiscoveryClient //服务发现
+@EnableDiscoveryClient //服务发现
 public class ProviderDept8001_App {
 
     public static void main(String[] args) {
