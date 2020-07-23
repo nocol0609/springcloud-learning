1bc67d5296828fa54d469a8b1e6e9539b5bc0c13
liuxp <liuxp@nala.com.cn>
2020-07-23 17:23:02 +0800

    feign

--- springcloud-consumer-dept-feign-6001/src/main/java/com/nocol/ConsumerDept_Feign6001_App.java
+++ springcloud-consumer-dept-feign-6001/src/main/java/com/nocol/ConsumerDept_Feign6001_App.java
@@ -3,18 +3,17 @@ package com.nocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient  //向服务中心注册
-public class ConsumerDept_Ribbon6001_App {
+//@EnableEurekaClient
+//@EnableDiscoveryClient  //向服务中心注册
+public class ConsumerDept_Feign6001_App {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerDept_Ribbon6001_App.class, args);
+        SpringApplication.run(ConsumerDept_Feign6001_App.class, args);
     }
 
 }
---
+++ springcloud-consumer-dept-feign-6001/src/main/java/com/nocol/controller/DeptController.java
@@ -0,0 +1,48 @@
+package com.nocol.controller;
+
+import com.nocol.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @author ：liuxp
+ * @date ：Created in 2019/8/26 16:52
+ * @description ：
+ */
+@RestController
+@RequestMapping("/ribbon")
+public class DeptController {
+
+    /**
+     * 对外暴露的服务名称
+     */
+    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @PostMapping(value = "/dept/add")
+    public boolean add(@RequestBody Dept dept) {
+        //return service.add(dept);
+        return Boolean.FALSE;
+    }
+
+    @GetMapping(value = "/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        //return service.get(id);
+        return null;
+    }
+
+    @GetMapping(value = "/dept/list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list",List.class);
+    }
+
+    @GetMapping(value = "hi")
+    public String hi(){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/hi",String.class);
+    }
+}
